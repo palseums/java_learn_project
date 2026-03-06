a52d6b72dc7314e2d85cc74965038f9d7151dc0c
palani <palaniappan.mca1@gmail.com>
2026-03-06 16:58:18 -0600

    My First commit

---
+++ src/com/learn/package1/Ourmain.java
@@ -0,0 +1,24 @@
+package com.learn.package1;
+
+public class Ourmain {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Our Main Program");
+		
+		String variable1 = "seetha";
+		String variable2 = "Palani";
+		
+		if ( variable1 != null && !(variable2 != null))
+		{
+			System.out.println("Not equal to null");
+			
+		}
+		else
+		{
+			System.out.println("Equal to null");
+		}
+
+	}
+
+}
